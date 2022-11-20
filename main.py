4f666dc608b3ce570d1c37b64a0f9de8a653dd63
Luuk van Campen <luuk.vanCampen@student.ru.nl>
2022-11-20 16:07:15 +0100

    Removed content type from /light/gettime

--- main.py
+++ main.py
@@ -166,8 +166,8 @@ class requestHandler(BaseHTTPRequestHandler):
             else:
                 global sunriseTime
                 reply = str(sunriseTime.hour) + ":" + str(sunriseTime.minute)
-                # self.send_header('Content-Type', 'application/json')
-                # self.end_headers()
+                self.send_header('Content-Type', 'application/json')
+                self.end_headers()
                 self.wfile.write(bytes(json.dumps({'time': reply}), 'UTF-8'))
                 self.send_response(200)
 
